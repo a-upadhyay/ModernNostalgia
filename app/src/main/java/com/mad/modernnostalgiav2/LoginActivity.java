c9d76825f5c6657a004ce5068fd94604bc77dc24
a-upadhyay <a.upadhyay279@gmail.com>
2018-06-12 02:10:52 +1000

    firebase auth working

--- app/src/main/java/com/mad/modernnostalgiav2/LoginActivity.java
+++ app/src/main/java/com/mad/modernnostalgiav2/LoginActivity.java
@@ -1,20 +1,119 @@
 package com.mad.modernnostalgiav2;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Activity for user authentication before accessing their content
  */
 public class LoginActivity extends DrawerActivity{
+    private static final String TAG = "MESSAGE" ;
+    private EditText mEmail, mPassword;
+    private ProgressBar mProgressBar;
+    private Button mSignIn, mRegister;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        mEmail = findViewById(R.id.et_login_user);
+        mPassword = findViewById(R.id.et_login_password);
+        mProgressBar = findViewById(R.id.reg_progress);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    private void loginUser() {
+        String email = mEmail.getText().toString().trim();
+        String pwd = mPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            mEmail.setError("Email required");
+            mEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmail.setError("Please enter a valid email address");
+            mEmail.requestFocus();
+            return;
+        }
+        if (pwd.isEmpty()) {
+            mPassword.setError("Password required");
+            mPassword.requestFocus();
+            return;
+        }
+        if (pwd.length()<6) {
+            mPassword.setError("Minimum 6 characters required");
+            mPassword.requestFocus();
+            return;
+        }
+
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                mProgressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success");
+                    Intent intent = new Intent(LoginActivity.this, AlbumActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //TODO check better way of doing this
+                    startActivity(intent);
+
+                    FirebaseUser user = mAuth.getCurrentUser();
+                } else {
+                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+    private void getCurrentUser() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            String uid = user.getUid();
+        }
     }
 
     /**
@@ -22,7 +121,10 @@ public class LoginActivity extends DrawerActivity{
      *  @param view
      */
     public void login(View view) {
-        Intent intent = new Intent(LoginActivity.this, AlbumActivity.class);
-        startActivity(intent);
+        loginUser();
+    }
+
+    public void registration(View view) {
+        startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
     }
 }
--- app/src/main/java/com/mad/modernnostalgiav2/RegistrationActivity.java
+++ app/src/main/java/com/mad/modernnostalgiav2/RegistrationActivity.java
@@ -1,22 +1,113 @@
 package com.mad.modernnostalgiav2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 /**
  * New Activity: Registration. This is for the user to register an account
  */
 public class RegistrationActivity extends DrawerActivity {
+    private static final String TAG = "MESSAGE" ;
+    private EditText mEmail, mPassword;
+    private ProgressBar mProgressBar;
+    private Button mSignUp, mReturn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_layout);
+
+        mEmail = findViewById(R.id.et_email);
+        mPassword = findViewById(R.id.et_password);
+        mProgressBar = findViewById(R.id.reg_progress);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mSignUp = findViewById(R.id.button_sign_up);
     }
 
-    public void cancelReg(View view) {
+    private void registerUser() {
+        String email = mEmail.getText().toString().trim();
+        String pwd = mPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            mEmail.setError("Email required");
+            mEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmail.setError("Please enter a valid email address");
+            mEmail.requestFocus();
+            return;
+        }
+        if (pwd.isEmpty()) {
+            mPassword.setError("Password required");
+            mPassword.requestFocus();
+            return;
+        }
+        if (pwd.length()<6) {
+            mPassword.setError("Minimum 6 characters required");
+            mPassword.requestFocus();
+            return;
+        }
+
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                mProgressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "createUserWithEmail:success");
+                    Toast.makeText(getApplicationContext(),"User registered",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                } else {
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                   /* if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                        Toast.makeText(getApplicationContext(), "Email already registered", Toast.LENGTH_SHORT).show(); } */
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
-    public void signUp(View view) {
+    public void registerUser(View view) {
+        registerUser();
     }
+
+    public void loginReturn(View view) {
+        Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+   /* @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_sign_up:
+
+                break;
+
+            case R.id.button_cancel:
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+
+        }
+    } */
 }
